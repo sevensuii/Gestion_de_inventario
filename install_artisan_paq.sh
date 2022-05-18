6b82036a0ab346ca4e37117713bdb8d0d1c5f1f1
sevensuii <severyn00@gmail.com>
2022-05-18 21:28:16 +0100

    Login con jetstream y orchid panel instalados

---
+++ install_artisan_paq.sh
@@ -0,0 +1,5 @@
+php artisan jetstream:install livewire
+php artisan orchid:install
+# creacion de usuario
+# php artisan orchid:admin admin admin@admin.com password
+
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,13 @@ Route::get('/', function () {
 
 
 URL::forceScheme('https');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
